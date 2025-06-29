b69c1e579dc098679f935b28d3a49f3785f13ae1
adham-hashem <adhamhashem2024@gmail.com>
2025-06-29 21:08:47 +0300

    feat(services interfaces): add lesson access code service interface

---
+++ Application/Services/Interfaces/ILessonAccessCodeService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Dtos.LessonAccessCodeDtos;
+using Application.Dtos.RedeemCodeDtos;
+
+namespace Application.Services.Interfaces
+{
+    public interface ILessonAccessCodeService
+    {
+        Task<LessonAccessCodeDto> GenerateCodeAsync(Guid lessonId, string teacherId);
+        Task<LessonAccessCodeDto> RedeemCodeAsync(RedeemCodeDto redeemCodeDto, string userId);
+        Task<List<LessonAccessCodeDto>> GetCodesByLessonAsync(Guid lessonId, string teacherId);
+        Task RevokeAccessAsync(Guid lessonId, string userId, string teacherId);
+        Task RevokeAllAccessAsync(Guid lessonId, string teacherId);
+    }
+}
