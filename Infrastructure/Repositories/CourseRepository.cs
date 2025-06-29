40bd838ed912cf2c85eaa70bfd755a8eb32955cb
adham-hashem <adhamhashem2024@gmail.com>
2025-06-29 04:23:21 +0300

    feat(repositories): add course repository implementation

---
+++ Infrastructure/Repositories/CourseRepository.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public class CourseRepository : ICourseRepository
+    {
+        private readonly AppDbContext _context;
+
+        public CourseRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Course> GetByIdAsync(Guid id)
+        {
+            return await _context.Courses
+                .Include(c => c.Lessons)
+                .Include(c => c.Reviews)
+                .FirstOrDefaultAsync(c => c.Id == id);
+        }
+
+        public async Task<List<Course>> GetAllAsync()
+        {
+            return await _context.Courses
+                .Include(c => c.Lessons)
+                .Include(c => c.Reviews)
+                .ToListAsync();
+        }
+
+        public async Task AddAsync(Course course)
+        {
+            await _context.Courses.AddAsync(course);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Course course)
+        {
+            _context.Courses.Update(course);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            var course = await _context.Courses.FindAsync(id);
+            if (course != null)
+            {
+                _context.Courses.Remove(course);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
