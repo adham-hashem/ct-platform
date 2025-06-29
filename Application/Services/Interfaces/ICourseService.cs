590d8a3b3ed10fee64218fefc36413fb6f5703c9
adham-hashem <adhamhashem2024@gmail.com>
2025-06-29 20:58:07 +0300

    feat(services interfaces): add course service interface

---
+++ Application/Services/Interfaces/ICourseService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Dtos.CourseDtos;
+
+namespace Application.Services.Interfaces
+{
+    public interface ICourseService
+    {
+        Task<CourseDto> GetByIdAsync(Guid id);
+        Task<List<CourseDto>> GetAllAsync();
+        Task<CourseDto> CreateAsync(CourseDto courseDto, string userId);
+        Task UpdateAsync(CourseDto courseDto, string userId);
+        Task DeleteAsync(Guid id, string userId);
+    }
+}
