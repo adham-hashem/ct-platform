dfcdaad9c112df88c9cf1baa5d1fde6d60b5026f
adham-hashem <adhamhashem2024@gmail.com>
2025-06-29 06:56:57 +0300

    feat(Dtos): add lesson access code dto

---
+++ Application/Dtos/LessonAccessCodeDtos/LessonAccessCodeDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Dtos.LessonAccessCodeDtos
+{
+    public class LessonAccessCodeDto
+    {
+        public Guid Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Code { get; set; } = string.Empty;
+        public Guid LessonId { get; set; }
+        public string? UserId { get; set; }
+        public bool IsUsed { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? RedeemedAt { get; set; }
+    }
+}
