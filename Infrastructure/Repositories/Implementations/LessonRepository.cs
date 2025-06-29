a29f3898268394aec3644db4c8c9a6731bfe6e0e
adham-hashem <adhamhashem2024@gmail.com>
2025-06-30 02:25:12 +0300

    feat(repositories implementations): add lesson repository implementation

---
+++ Infrastructure/Repositories/Implementations/LessonRepository.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories.Implementations
+{
+    public class LessonRepository : ILessonRepository
+    {
+        private readonly AppDbContext _context;
+
+        public LessonRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Lesson> GetByIdAsync(Guid id)
+        {
+            return await _context.Lessons
+                .Include(l => l.Course)
+                .FirstOrDefaultAsync(l => l.Id == id);
+        }
+
+        public async Task<bool> HasAccessCodeAsync(Guid lessonId, string userId)
+        {
+            return await _context.LessonAccessCodes
+                .AnyAsync(lac => lac.LessonId == lessonId && lac.UserId == userId && lac.IsUsed);
+        }
+
+        public async Task AddAsync(Lesson lesson)
+        {
+            await _context.Lessons.AddAsync(lesson);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Lesson lesson)
+        {
+            _context.Lessons.Update(lesson);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            var lesson = await _context.Lessons.FindAsync(id);
+            if (lesson != null)
+            {
+                _context.Lessons.Remove(lesson);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
