1977a9ced19a3582589cae4d8b6466e5cadfcf1b
adham-hashem <adhamhashem2024@gmail.com>
2025-06-29 06:53:29 +0300

    feat(Dtos): add subscription dto

---
+++ Application/Dtos/SubscriptionDtos/SubscriptionDto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Dtos.SubscriptionDtos
+{
+    public class SubscriptionDto
+    {
+        public Guid Id { get; set; }
+        
+        [Required]
+        public string UserId { get; set; }
+        
+        [Required]
+        public string Type { get; set; } = string.Empty;
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        
+        [Required]
+        public List<int> SubscribedMonths { get; set; } = new List<int>();
+        
+        [Required]
+        public string Grade { get; set; } = string.Empty;
+        public int? LectureCount { get; set; }
+
+        [Required]
+        [Range(0, double.MaxValue)]
+        public decimal Price { get; set; }
+        public List<Guid> AccessedLessons { get; set; } = new List<Guid>();
+    }
+}
