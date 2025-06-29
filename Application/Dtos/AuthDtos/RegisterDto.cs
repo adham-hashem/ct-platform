e5bd11cec028e46ad75ef50afedd62c4baf48d26
adham-hashem <adhamhashem2024@gmail.com>
2025-06-29 21:13:50 +0300

    feat(Dtos): add register dto

---
+++ Application/Dtos/AuthDtos/RegisterDto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Dtos.AuthDtos
+{
+    public class RegisterDto
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [MinLength(8)]
+        public string Password { get; set; } = string.Empty;
+
+        [Required]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required]
+        public string LastName { get; set; } = string.Empty;
+
+        [Required]
+        public string Role { get; set; } = string.Empty;
+
+        [Required]
+        public string Grade { get; set; } = string.Empty;
+    }
+}
