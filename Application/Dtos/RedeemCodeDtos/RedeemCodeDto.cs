41bf01f947637bef263c081b388e5064befbd3a6
adham-hashem <adhamhashem2024@gmail.com>
2025-06-29 07:09:23 +0300

    feat(Dtos): add redeem code dto

---
+++ Application/Dtos/RedeemCodeDtos/RedeemCodeDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Dtos.RedeemCodeDtos
+{
+    public class RedeemCodeDto
+    {
+        [Required]
+        [StringLength(50)]
+        public string Code { get; set; } = string.Empty;
+    }
+}
