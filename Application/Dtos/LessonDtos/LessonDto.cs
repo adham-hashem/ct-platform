fc4a48ec23150e21645b94c3b41dc765ffd6bfca
adham-hashem <adhamhashem2024@gmail.com>
2025-06-29 06:29:55 +0300

    feat(DTOs): add lesson dto

---
+++ Application/Dtos/LessonDtos/LessonDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Dtos.LessonDtos
+{
+    public class LessonDto
+    {
+        public Guid Id { get; set; }
+        [Required]
+        public string Title { get; set; } = string.Empty;
+        [Required]
+        public string VideoUrl { get; set; } = string.Empty;
+        public bool IsFree { get; set; }
+        public int MonthAssigned { get; set; }
+        public Guid CourseId { get; set; }
+    }
+}
