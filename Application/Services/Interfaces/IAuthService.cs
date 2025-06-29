eb43bfc55732aac153b9a4bd4f7495d1babbacdd
adham-hashem <adhamhashem2024@gmail.com>
2025-06-29 21:23:36 +0300

    feat(services interfaces): add auth service interface

---
+++ Application/Services/Interfaces/IAuthService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Dtos.AuthDtos;
+
+namespace Application.Services.Interfaces
+{
+    public interface IAuthService
+    {
+        Task<string> RegisterAsync(RegisterDto registerDto);
+        Task<string> LoginAsync(LoginDto loginDto);
+    }
+}
