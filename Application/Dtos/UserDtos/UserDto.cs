f41b2f58b14801abf7def53fd72d4a36daacbc12
adham-hashem <adhamhashem2024@gmail.com>
2025-06-29 07:11:10 +0300

    feat(Dtos): add user dto

---
+++ Application/Dtos/UserDtos/UserDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Dtos.UserDtos
+{
+    public class UserDto
+    {
+        public string Id { get; set; }
+        
+        [Required]
+        public string Email { get; set; }
+        
+        [Required]
+        public string FirstName { get; set; }
+        
+        [Required]
+        public string LastName { get; set; }
+        
+        [Required]
+        public string Grade { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
