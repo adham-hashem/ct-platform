1b741d893f3cd226589685cc836b95767ea8c3aa
adham-hashem <adhamhashem2024@gmail.com>
2025-06-29 21:00:45 +0300

    feat(services implementations): add subscription service interface

---
+++ Application/Services/Interfaces/ISubscriptionService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Dtos.SubscriptionDtos;
+
+namespace Application.Services.Interfaces
+{
+    public interface ISubscriptionService
+    {
+        Task<SubscriptionDto> CreateSubscriptionAsync(SubscriptionDto subscriptionDto, string userId);
+        Task<SubscriptionDto> CreateLectureBasedSubscriptionAsync(SubscriptionDto subscriptionDto, string teacherId);
+        Task<bool> CanAccessLessonAsync(string userId, Guid lessonId);
+        Task MarkLessonAsAccessedAsync(string userId, Guid lessonId);
+        Task DeleteUserSubscriptionsAsync(string userId, string teacherId);
+        Task DeleteAllSubscriptionsAsync(string teacherId);
+    }
+}
