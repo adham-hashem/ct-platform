1daf2ec108bc7669f6a4b012cbdbc3d2c7c8484c
adham-hashem <adhamhashem2024@gmail.com>
2025-07-01 22:23:13 +0300

    refactor(entities): update lesson entity

--- Domain/Entities/Lesson.cs
+++ Domain/Entities/Lesson.cs
@@ -10,8 +10,11 @@ namespace Domain.Entities
     public class Lesson
     {
         public Guid Id { get; set; }
-        public string Title { get; set; }
-        public string VideoUrl { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string VideoUrl { get; set; } = string.Empty
+        public string LessonSummaryText { get; set; } = string.Empty;
+        public string LessonSummaryPdfPath { get; set; } = string.Empty;
+        public string EquationsTablePdfPath { get; set; } = string.Empty;
         public bool IsFree { get; set; }
         public int? MonthAssigned { get; set; }
         public string? AdditionalResources { get; set; }
