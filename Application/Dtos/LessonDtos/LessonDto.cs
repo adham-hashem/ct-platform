783e944aff7cef3f138ccb82b49ed59dcf455a94
adham-hashem <adhamhashem2024@gmail.com>
2025-07-01 22:25:18 +0300

    refactor(Dtos): update lesson dto

--- Application/Dtos/LessonDtos/LessonDto.cs
+++ Application/Dtos/LessonDtos/LessonDto.cs
@@ -4,20 +4,23 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 
 namespace Application.Dtos.LessonDtos
 {
     public class LessonDto
     {
         public Guid Id { get; set; }
-
-        [Required]
         public string Title { get; set; } = string.Empty;
-        
-        [Required]
         public string VideoUrl { get; set; } = string.Empty;
+        public string LessonSummaryText { get; set; } = string.Empty;
+        public IFormFile? LessonSummaryPdf { get; set; }
+        public IFormFile? EquationsTablePdf { get; set; }
+        public string? LessonSummaryPdfPath { get; set; };
+        public string? EquationsTablePdfPath { get; set; };
         public bool IsFree { get; set; }
-        public int MonthAssigned { get; set; }
+        public int? MonthAssigned { get; set; }
+        public string? AdditionalResources { get; set; }
         public Guid CourseId { get; set; }
     }
 }
