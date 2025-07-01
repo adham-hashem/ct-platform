25221efb48c42a37581f9ddd478aeab56968dc05
adham-hashem <adhamhashem2024@gmail.com>
2025-07-01 18:30:24 +0300

    refactor(entities): update application user entity

--- Domain/Entities/ApplicationUser.cs
+++ Domain/Entities/ApplicationUser.cs
@@ -12,33 +12,21 @@ namespace Domain.Entities
 {
     public class ApplicationUser : IdentityUser
     {
-        [Required]
-        [StringLength(50)]
-        public string FirstName { get; set; } = string.Empty;
-
-        [Required]
-        [StringLength(50)]
-        public string LastName { get; set; } = string.Empty;
-
-        [StringLength(500)]
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string? Bio { get; set; }
-
-        [StringLength(1000)]
         public string? Qualifications { get; set; }
-
-        [Phone]
-        public string? PhoneNumber { get; set; }
-
-        public EducationalLevel? Grade { get; set; } // FirstYear, SecondYear, ThirdYear
+        public EducationalLevel? Grade { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime RegistrationDate { get; set; }
         public DateTime? LastActiveDate { get; set; }
         public bool IsOnline { get; set; }
         public double VideoCompletionRate { get; set; }
-        public List<Payment> Payments { get; set; } = new List<Payment>();
         public List<Subscription> Subscriptions { get; set; } = new List<Subscription>();
         public List<Review> Reviews { get; set; } = new List<Review>();
         public List<Comment> Comments { get; set; } = new List<Comment>();
         public List<Question> Questions { get; set; } = new List<Question>();
+        public List<Payment> Payments { get; set; } = new List<Payment>();
+        public List<ExamResult> ExamResults { get; set; } = new List<ExamResult>();
     }
 }
